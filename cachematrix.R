721036505782f8075e2c1515b85d5e4bbc1e1d12
alexolson-umd <54953725+alexolson-umd@users.noreply.github.com>
2019-09-05 15:57:04 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Together, these functions allow the user to cache the inverse 
+## of a matrix (calculated by solve()) so that it can be called 
+## later to save computing time.
 
-## Write a short comment describing this function
+# makeCacheMatrix creates an environment where "x" is the input
+# matrix and "inv" is the inverse of x. The four functions within
+# this environment are $set, $get, $setinv, and $getinv, returned 
+# as a list. $set and $setinv set the values of x and inv, while 
+# $get and $getinv retrieve the cached values of x and inv.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
+# cacheSolve retrieves the value of inv from makeCacheMatrix, if 
+# present, by calling $getinv(). If not, it calculates the inverse 
+# of x and sets this as inv within makeCacheMatrix with $setinv().
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cache, ...) {
+  inv <- cache$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- cache$get()
+  inv <- solve(data, ...)
+  cache$setinv(inv)
+  inv
 }
